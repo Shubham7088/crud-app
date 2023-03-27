41b754dfba3fdc1dd29020af2f16d681e5dd2b8c
Shubham7088 <111425217+Shubham7088@users.noreply.github.com>
2023-03-27 13:49:18 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { ListStudentComponent } from './list-student/list-student.component';
+
+const routes: Routes = [
+  {
+    path:'', component:AddStudentComponent
+  },
+  { path: 'add-student', component:AddStudentComponent},
+  { path: 'list-student', component:ListStudentComponent},
+  { path: 'edit-student/:id', component:EditStudentComponent}
+  
+  
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
